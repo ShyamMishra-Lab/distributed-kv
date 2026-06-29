9d0cafcba374a6a8f3ee0a65d4b4d99c7a58397c
Shyam Mishra <108shyam.mishra@gmail.com>
2026-06-30 01:44:47 +0530

    TestOpenExistingFile

--- file/manager_test.go
+++ file/manager_test.go
@@ -1,1 +1,37 @@
 package file
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"testing"
+)
+
+func TestOpenExistingFile(t *testing.T) {
+	//Arrange
+	dir := t.TempDir()
+	dbPath := filepath.Join(dir, "test.db")
+	f, err := os.Create(dbPath)
+	if err != nil {
+		t.Fatalf("Error on file creation: %s", err)
+	}
+	
+	if err := f.Close; err != nil {
+		t.Fatalf("failed to close temporary file: %v", err)
+	}
+
+	//Opening file manager
+	fm := NewFileManager(dbPath)
+
+	//Act
+	err = fm.Open()
+
+	//Assert
+	if err != nil {
+		t.Fatalf("error on fm.Open: %v", err)
+	}
+
+	if fm.file == nil {
+		t.Fatalf("expected fm.file to be not nil")
+	}
+}
