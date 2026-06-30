358c6276b115ead6f809e23e975b9004214593d5
Shyam Mishra <108shyam.mishra@gmail.com>
2026-06-30 09:52:18 +0530

    test fix: TestOpenExistingFile

--- file/manager_test.go
+++ file/manager_test.go
@@ -1,7 +1,7 @@
 package file
 
 import (
-	"fmt"
+
 	"os"
 	"path/filepath"
 	"testing"
@@ -13,15 +13,23 @@ func TestOpenExistingFile(t *testing.T) {
 	dbPath := filepath.Join(dir, "test.db")
 	f, err := os.Create(dbPath)
 	if err != nil {
-		t.Fatalf("Error on file creation: %s", err)
+		t.Fatalf("Error on file creation: %v", err)
 	}
 	
-	if err := f.Close; err != nil {
+	if err := f.Close(); err != nil {
 		t.Fatalf("failed to close temporary file: %v", err)
 	}
 
 	//Opening file manager
 	fm := NewFileManager(dbPath)
+	//closing the file manager at last
+	//error if not close---The process cannot access the file because it is being used by another process.
+	//it comes as the file inside the temp dir is not closed while windows try to clear temp dir
+	defer func() {
+		if err := fm.Close(); err != nil && err != ErrFileNotOpen {
+			t.Fatalf("error on fm.Close: %v", err)
+		}
+	}()
 
 	//Act
 	err = fm.Open()
